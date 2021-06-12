f43230c1657c4b0c13422244fdf4a9363bf8b39a
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2021-06-12 12:31:55 -0700

    gtm install

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "react-slideshow-image/dist/styles.css";
 import Loader from "./components/Loader";
 import { load_timer } from "./utils/timers";
-import "./utils/desktop-css.css"
-import "./utils/iphone5-css.css"
-import "./utils/iphone8-css.css"
-import "./utils/iphoneX-css.css"
-import "./utils/ipad-css.css"
-import "./utils/general-css.css"
-import "./utils/transition-css.css"
+import "./utils/desktop-css.css";
+import "./utils/iphone5-css.css";
+import "./utils/iphone8-css.css";
+import "./utils/iphoneX-css.css";
+import "./utils/ipad-css.css";
+import "./utils/general-css.css";
+import "./utils/transition-css.css";
 
-
+import TagManager from "react-gtm-module";
 
 const Home = lazy(() => import("./pages/Home"));
 
@@ -20,6 +20,11 @@ function App() {
   useEffect(() => {
     setTimeout(() => setLoading(false), load_timer);
   }, []);
+  const tagManagerArgs = {
+    gtmId: "GTM-N5D3GQX",
+  };
+
+  TagManager.initialize(tagManagerArgs);
   return (
     <Router>
       <Suspense fallback={<Loader />}>
