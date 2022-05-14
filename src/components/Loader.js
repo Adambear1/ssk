5c48e8a806c9ebcfc9e253a3745e04730b9923ab
adambear1 <adamcarlbirgenheier@gmail.com>
2022-05-14 12:14:26 -0500

    updated mobile responsiveness for loader text

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,9 +3,10 @@ import { loader } from "../utils/css";
 import "../utils/general-css.css"
 
 function Loader() {
+  const text = window.screen.width > 750 ? "#SEVENSONSKITCHEN" : "#SSK"
   return (
     <div className="loader animate__animated animate__fadeIn" style={loader.loader_logo}>
-      <h2 className="animate" style={loader.loader_animate}>#SEVENSONSKITCHEN</h2>
+      <h2 className="animate" style={loader.loader_animate}>{text}</h2>
     </div>
   );
 }
