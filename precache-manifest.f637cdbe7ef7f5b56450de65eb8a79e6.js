1a18ea32eab18dc7663a2d5afeb9452bd18c3a17
adambear1 <adamcarlbirgenheier@gmail.com>
2022-11-10 19:15:10 -0600

    Updates

---
+++ precache-manifest.f637cdbe7ef7f5b56450de65eb8a79e6.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b291983be4a280771b8",
+    "url": "/static/css/main.6ec88f9b.chunk.css"
+  },
+  {
+    "revision": "1b291983be4a280771b8",
+    "url": "/static/js/main.ee8676eb.chunk.js"
+  },
+  {
+    "revision": "db317fa7bacbc0c230db",
+    "url": "/static/js/runtime~main.9b18c990.js"
+  },
+  {
+    "revision": "8575a447358b342aa762",
+    "url": "/static/js/2.96196806.chunk.js"
+  },
+  {
+    "revision": "c0fdf4041f01ade9319c",
+    "url": "/static/js/3.bd0737ad.chunk.js"
+  },
+  {
+    "revision": "760e045aebd83afb6667",
+    "url": "/static/js/4.f42cbd46.chunk.js"
+  },
+  {
+    "revision": "c79a00f0a8f25c64c61ab5b224046ad0",
+    "url": "/static/media/chicken6.c79a00f0.jpg"
+  },
+  {
+    "revision": "099285db1830062b5d2166fe9f3e5169",
+    "url": "/static/media/burnttips1.099285db.jpg"
+  },
+  {
+    "revision": "57996001e6556cbc7b6efe9d5e83429c",
+    "url": "/static/media/ribs3.57996001.jpg"
+  },
+  {
+    "revision": "9c1d159b23509a98dcbc5c530a1b61ef",
+    "url": "/static/media/slider1.9c1d159b.jpg"
+  },
+  {
+    "revision": "2c41088a0f1839bd75711785f2169a15",
+    "url": "/static/media/logo.2c41088a.png"
+  },
+  {
+    "revision": "9c205de6e15e2f96daffeef1a5c1b343",
+    "url": "/static/media/meats_vegges.9c205de6.jpg"
+  },
+  {
+    "revision": "dafc5b8a14f737ce68be5937b8d85b5b",
+    "url": "/static/media/side-min.dafc5b8a.png"
+  },
+  {
+    "revision": "461cbba0d2c616a4b3112496950b7d2c",
+    "url": "/static/media/chicken1.461cbba0.jpg"
+  },
+  {
+    "revision": "823bc427bda0c31789b17fa3479af8bb",
+    "url": "/static/media/chicken2.823bc427.jpg"
+  },
+  {
+    "revision": "cebd661d23b96d9a6b1f8156eb75e1de",
+    "url": "/static/media/chicken3.cebd661d.jpg"
+  },
+  {
+    "revision": "57996001e6556cbc7b6efe9d5e83429c",
+    "url": "/static/media/chicken4.57996001.jpg"
+  },
+  {
+    "revision": "e6cc87bc7566e49e3639ae69a80c07dc",
+    "url": "/static/media/chicken5.e6cc87bc.jpg"
+  },
+  {
+    "revision": "598c9d65bec2b5e51cc8116426bbb2b0",
+    "url": "/static/media/chicken7.598c9d65.jpg"
+  },
+  {
+    "revision": "dcabb815d21c441d9d8cf0ea0359335b",
+    "url": "/static/media/links1.dcabb815.jpg"
+  },
+  {
+    "revision": "8185c3c90b52a0d1cbbb8a7d711bfbfc",
+    "url": "/static/media/links2.8185c3c9.png"
+  },
+  {
+    "revision": "29d63d705e1b2cc6df3e86f15c4cbba1",
+    "url": "/static/media/links3.29d63d70.jpg"
+  },
+  {
+    "revision": "6fdf94d42c8f995c4d418ea60843fbcb",
+    "url": "/static/media/links4.6fdf94d4.jpg"
+  },
+  {
+    "revision": "fbf418ad9d033ea2e27b74f92d608372",
+    "url": "/static/media/ribs1.fbf418ad.png"
+  },
+  {
+    "revision": "a01a5722d9fd67910408a69e31a43dfe",
+    "url": "/static/media/ribs2.a01a5722.jpg"
+  },
+  {
+    "revision": "2f97a7e79bfc4572bea0f88606e6a807",
+    "url": "/static/media/burnttips2.2f97a7e7.jpg"
+  },
+  {
+    "revision": "daf9509616a756d85fc90943ee6ec365",
+    "url": "/static/media/burnttips3.daf95096.jpg"
+  },
+  {
+    "revision": "1c83050417e30a118c828d48be168083",
+    "url": "/static/media/gumbo1.1c830504.jpg"
+  },
+  {
+    "revision": "fc6a42c63e5141f545c77efd0165e514",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a314fe6e68d854f8c76d7fe7b04b7442.js"
+  "/precache-manifest.f637cdbe7ef7f5b56450de65eb8a79e6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
